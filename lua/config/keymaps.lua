5eb7a81e3da0a78d92c230f119e2fe0b305eb70b
Joshua Wornyuie Ketor <joshuaketor@proton.me>
2025-10-07 07:37:38 +0000

    Add keymaps to center screen when jumping

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,10 @@
+-- ================================================================================================
+-- TITLE: NeoVim keymaps
+-- ABOUT: sets some quality-of-life keymaps
+-- ================================================================================================
+
+-- Center screen when jumping
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previouse search result (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
