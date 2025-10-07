dc39a6154dd179d9ba106c110dc2d3d97998f18d
Joshua Wornyuie Ketor <joshuaketor@proton.me>
2025-10-07 07:59:11 +0000

    Add keymap for quick config editing

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -34,4 +34,6 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
 -- Better J behaviour
 vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
 
+ -- Quick config editing
+vim.keymap.set("n", "<leader>rc", "<Cmd>e ~/.config/nvim/init.lua<CR>", { desc = "Edit config" })
 
